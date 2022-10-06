558829d3972726c9b2f0abb858c9dc05a8056405
lam <94112608+hjh-sss@users.noreply.github.com>
2022-10-06 16:11:23 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "送给你",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "今天的寿星",  // 同上...
+        "今天是你的生日",
+        "愿你是清新的海风，鼓起白色的船帆",
+    ],
+    imgs: {
+        "心爱的小可爱": "./imgs/touxiang.jpg",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
+    }
+};
